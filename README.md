167e0cc248f06cd4eaf2e48cce4956fadc508958
Olga <hloesilent@gmail.com>
2019-02-28 18:43:53 +0300

    html and start css done

---
+++ README.md
@@ -0,0 +1,2 @@
+# Zet
+a easy site
---
+++ source/js/script.js
@@ -0,0 +1,15 @@
+'use strict';
+/*video */
+/*
+  <script src="js/backend.js"></script>
+  <script src="js/menu.js"></script>
+  <script src="js/debounce.js"></script>
+  <script src="js/form.js"></script>
+  <script src="js/console.js"></script>
+*/
+(function () {
+
+})();
+
+
+/* */
