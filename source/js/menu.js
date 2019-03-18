95521dd8c5cac477d5e4355357066165dc5fd2e7
Olga <hloesilent@gmail.com>
2019-03-18 17:18:16 +0300

    fix header, still have problem with background-color of nav, nedd to add animation

--- source/js/menu.js
+++ source/js/menu.js
@@ -12,43 +12,54 @@
 
   var toggle = document.querySelector(".nav-toggle");
   var menu = document.querySelector(".nav__list");
-  var navElement = document.querySelector(".nav");
-  var sticky = navElement.offsetTop;
+  var header = document.querySelector(".header");
+  var sticky = header.offsetTop;
 
   var toggleDisplayMenu = function (e) {
+
     e.preventDefault();
+
     if (toggle.classList.contains("open")) {
-      console.log("close menu!");
+      console.log("menu is open now");
       toggle.classList.remove("open");
       toggle.classList.add("close");
       menu.classList.add("nav__list--show");
 
     } else {
-      console.log("show menu!");
-      toggle.classList.remove("close");
+      console.log("menu has been closed");
       toggle.classList.add("open");
+      toggle.classList.remove("close");
       menu.classList.remove("nav__list--show");
-
     }
-
   };
 
   var stickNavtoTop = function () {
     if (window.pageYOffset > sticky) {
-      navElement.classList.add("sticky");
-      menu.classList.add("sticky-show");
-      // console.log("navElement.scrollTop is " + navElement.scrollTop);
+      header.classList.add("sticky-show");
+      if (window.innerWidth < 935) {
+        menu.classList.add("sticky-show");
+      }
+
+      // console.log("header.scrollTop is " + header.scrollTop);
       // console.log("window.pageYOffset is " + window.pageYOffset);
       // console.log("sticky is " + sticky);
     } else {
-      navElement.classList.remove("sticky");
+      header.classList.remove("sticky-show");
       menu.classList.remove("sticky-show");
     }
   }
 
+  stickNavtoTop();
+  window.addEventListener("scroll", stickNavtoTop);
 
   toggle.addEventListener("touchstart", toggleDisplayMenu);
   toggle.addEventListener("click", toggleDisplayMenu);
 
-  window.addEventListener("scroll", stickNavtoTop)
+
+  toggle.addEventListener("click", function () {
+    console.log("it has been clicked!!");
+  });
+
+
+
 })();
